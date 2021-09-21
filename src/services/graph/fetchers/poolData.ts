1f1c6c9fc14c7184d16cf16de5ef112506bb6bc9
agelessZeal <35507996+agelessZeal@users.noreply.github.com>
2021-09-21 18:30:02 +0300

    add current pancake streamingfast subgraph api and function add

---
+++ src/services/graph/fetchers/poolData.ts
@@ -0,0 +1,497 @@
+/* eslint-disable no-param-reassign */
+import { useEffect, useState } from 'react'
+import { request, gql } from 'graphql-request'
+// import { INFO_CLIENT } from 'config/constants/endpoints'
+// import { getDeltaTimestamps } from 'views/Info/utils/infoQueryHelpers'
+// import { useBlocksFromTimestamps } from 'views/Info/hooks/useBlocksFromTimestamps'
+// import { PoolData } from 'state/info/types'
+// import { getChangeForPeriod, getLpFeesAndApr, getPercentChange } from 'views/Info/utils/infoDataHelpers'
+
+import { getUnixTime, subDays, subWeeks, startOfMinute } from 'date-fns'
+
+export const MINIMUM_SEARCH_CHARACTERS = 2
+
+export const WEEKS_IN_YEAR = 52.1429
+
+export const TOTAL_FEE = 0.0025
+export const LP_HOLDERS_FEE = 0.0017
+export const TREASURY_FEE = 0.0003
+export const BUYBACK_FEE = 0.0005
+
+export const PCS_V2_START = 1619136000 // April 23, 2021, 12:00:00 AM
+export const ONE_DAY_UNIX = 86400 // 24h * 60m * 60s
+export const ONE_HOUR_SECONDS = 3600
+
+export const ITEMS_PER_INFO_TABLE_PAGE = 10
+
+/**
+ * Get increase/decrease of value compared to the previous value (e.g. 24h volume compared to 24h volume the day before )
+ * @param valueNow - more recent value
+ * @param valueBefore - value to compare with
+ */
+export const getAmountChange = (valueNow?: number, valueBefore?: number) => {
+  if (valueNow && valueBefore) {
+    return valueNow - valueBefore
+  }
+  if (valueNow) {
+    return valueNow
+  }
+  return 0
+}
+
+/**
+ * Get increase/decrease of value compared to the previous value as a percentage
+ * @param valueNow - more recent value
+ * @param valueBefore - value to compare with
+ */
+export const getPercentChange = (valueNow?: number, valueBefore?: number): number => {
+  if (valueNow && valueBefore) {
+    return ((valueNow - valueBefore) / valueBefore) * 100
+  }
+  return 0
+}
+
+/**
+ * Given current value and value 1 and 2 periods (e.g. 1day + 2days, 1week - 2weeks) returns the amount change for latest period
+ * and percentage change compared to the previous period.
+ * @param valueNow - current value
+ * @param valueOnePeriodAgo - value 1 period ago (e.g. 1 day or 1 week ago), period unit must be same as valueTwoPeriodsAgo
+ * @param valueTwoPeriodsAgo - value 2 periods ago (e.g. 2 days or 2 weeks ago), period unit must be same as valueOnePeriodAgo
+ * @returns amount change for the latest period and percetage change compared to previous period
+ */
+export const getChangeForPeriod = (
+  valueNow?: number,
+  valueOnePeriodAgo?: number,
+  valueTwoPeriodsAgo?: number
+): [number, number] => {
+  const currentPeriodAmount = getAmountChange(valueNow, valueOnePeriodAgo)
+  const previousPeriodAmount = getAmountChange(valueOnePeriodAgo, valueTwoPeriodsAgo)
+  const percentageChange = getPercentChange(currentPeriodAmount, previousPeriodAmount)
+  return [currentPeriodAmount, percentageChange]
+}
+
+export const getLpFeesAndApr = (volumeUSD: number, volumeUSDWeek: number, liquidityUSD: number) => {
+  const totalFees24h = volumeUSD * TOTAL_FEE
+  const totalFees7d = volumeUSDWeek * TOTAL_FEE
+  const lpFees24h = volumeUSD * LP_HOLDERS_FEE
+  const lpFees7d = volumeUSDWeek * LP_HOLDERS_FEE
+
+  const lpApr7d = liquidityUSD > 0 ? (volumeUSDWeek * LP_HOLDERS_FEE * WEEKS_IN_YEAR * 100) / liquidityUSD : 0
+  return {
+    totalFees24h,
+    totalFees7d,
+    lpFees24h,
+    lpFees7d,
+    lpApr7d: lpApr7d !== Infinity ? lpApr7d : 0,
+  }
+}
+
+export const BLOCKS_CLIENT = 'https://api.thegraph.com/subgraphs/name/pancakeswap/blocks'
+
+export interface Block {
+  number: number
+  timestamp: string
+}
+
+export const getDeltaTimestamps = (): [number, number, number, number] => {
+  const utcCurrentTime = getUnixTime(new Date()) * 1000
+  const t24h = getUnixTime(startOfMinute(subDays(utcCurrentTime, 1)))
+  const t48h = getUnixTime(startOfMinute(subDays(utcCurrentTime, 2)))
+  const t7d = getUnixTime(startOfMinute(subWeeks(utcCurrentTime, 1)))
+  const t14d = getUnixTime(startOfMinute(subWeeks(utcCurrentTime, 2)))
+  return [t24h, t48h, t7d, t14d]
+}
+
+/**
+ * Helper function to get large amount GrqphQL subqueries
+ * @param queryConstructor constructor function that combines subqueries
+ * @param subqueries individual queries
+ * @param endpoint GraphQL endpoint
+ * @param skipCount how many subqueries to fire at a time
+ * @returns
+ */
+export const multiQuery = async (
+  queryConstructor: (subqueries: string[]) => string,
+  subqueries: string[],
+  endpoint: string,
+  skipCount = 1000
+) => {
+  let fetchedData = {}
+  let allFound = false
+  let skip = 0
+  try {
+    while (!allFound) {
+      let end = subqueries.length
+      if (skip + skipCount < subqueries.length) {
+        end = skip + skipCount
+      }
+      const subqueriesSlice = subqueries.slice(skip, end)
+      // eslint-disable-next-line no-await-in-loop
+      const result = await request(endpoint, queryConstructor(subqueriesSlice))
+      fetchedData = {
+        ...fetchedData,
+        ...result,
+      }
+      allFound = Object.keys(result).length < skipCount || skip + skipCount > subqueries.length
+      skip += skipCount
+    }
+    return fetchedData
+  } catch (error) {
+    console.error('Failed to fetch info data', error)
+    return null
+  }
+}
+
+const getBlockSubqueries = (timestamps: number[]) =>
+  timestamps.map((timestamp) => {
+    return `t${timestamp}:blocks(first: 1, orderBy: timestamp, orderDirection: desc, where: { timestamp_gt: ${timestamp}, timestamp_lt: ${
+      timestamp + 600
+    } }) {
+      number
+    }`
+  })
+
+const blocksQueryConstructor = (subqueries: string[]) => {
+  return gql`query blocks {
+    ${subqueries}
+  }`
+}
+
+export const getBlocksFromTimestamps = async (
+  timestamps: number[],
+  sortDirection: 'asc' | 'desc' = 'desc',
+  skipCount = 500
+): Promise<Block[]> => {
+  if (timestamps?.length === 0) {
+    return []
+  }
+
+  const fetchedData: any = await multiQuery(
+    blocksQueryConstructor,
+    getBlockSubqueries(timestamps),
+    BLOCKS_CLIENT,
+    skipCount
+  )
+
+  const sortingFunction =
+    sortDirection === 'desc' ? (a: Block, b: Block) => b.number - a.number : (a: Block, b: Block) => a.number - b.number
+
+  const blocks: Block[] = []
+  if (fetchedData) {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key of Object.keys(fetchedData)) {
+      if (fetchedData[key].length > 0) {
+        blocks.push({
+          timestamp: key.split('t')[1],
+          number: parseInt(fetchedData[key][0].number, 10),
+        })
+      }
+    }
+    // graphql-request does not guarantee same ordering of batched requests subqueries, hence manual sorting
+    blocks.sort(sortingFunction)
+  }
+  return blocks
+}
+
+export const useBlocksFromTimestamps = (
+  timestamps: number[],
+  sortDirection: 'asc' | 'desc' = 'desc',
+  skipCount = 1000
+): {
+  blocks?: Block[]
+  error: boolean
+} => {
+  const [blocks, setBlocks] = useState<Block[]>()
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getBlocksFromTimestamps(timestamps, sortDirection, skipCount)
+      if (result.length === 0) {
+        setError(true)
+      } else {
+        setBlocks(result)
+      }
+    }
+    if (!blocks && !error) {
+      fetchData()
+    }
+  }) // TODO: dep array?
+
+  return {
+    blocks,
+    error,
+  }
+}
+
+export const INFO_CLIENT = 'https://bsc.streamingfast.io/subgraphs/name/pancakeswap/exchange-v2'
+
+// POOLS
+
+export interface PoolData {
+  address: string
+
+  token0: {
+    name: string
+    symbol: string
+    address: string
+  }
+
+  token1: {
+    name: string
+    symbol: string
+    address: string
+  }
+
+  volumeUSD: number
+  volumeUSDChange: number
+  volumeUSDWeek: number
+  volumeUSDChangeWeek: number
+
+  totalFees24h: number
+  totalFees7d: number
+  lpFees24h: number
+  lpFees7d: number
+  lpApr7d: number
+
+  liquidityUSD: number
+  liquidityUSDChange: number
+
+  token0Price: number
+  token1Price: number
+
+  liquidityToken0: number
+  liquidityToken1: number
+}
+
+interface PoolFields {
+  id: string
+  reserve0: string
+  reserve1: string
+  reserveUSD: string
+  volumeUSD: string
+  token0Price: string
+  token1Price: string
+  token0: {
+    id: string
+    symbol: string
+    name: string
+  }
+  token1: {
+    id: string
+    symbol: string
+    name: string
+  }
+}
+
+interface FormattedPoolFields
+  extends Omit<PoolFields, 'volumeUSD' | 'reserveUSD' | 'reserve0' | 'reserve1' | 'token0Price' | 'token1Price'> {
+  volumeUSD: number
+  reserveUSD: number
+  reserve0: number
+  reserve1: number
+  token0Price: number
+  token1Price: number
+}
+
+interface PoolsQueryResponse {
+  now: PoolFields[]
+  oneDayAgo: PoolFields[]
+  twoDaysAgo: PoolFields[]
+  oneWeekAgo: PoolFields[]
+  twoWeeksAgo: PoolFields[]
+}
+
+/**
+ * Data for displaying pool tables (on multiple pages, used throughout the site)
+ * Note: Don't try to refactor it to use variables, server throws error if blocks passed as undefined variable
+ * only works if its hard-coded into query string
+ */
+const POOL_AT_BLOCK = (block: number | null, pools: string[]) => {
+  const blockString = block ? `block: {number: ${block}}` : ``
+  const addressesString = `["${pools.join('","')}"]`
+  return `pairs(
+    where: { id_in: ${addressesString} }
+    ${blockString}
+    orderBy: trackedReserveBNB
+    orderDirection: desc
+  ) {
+    id
+    reserve0
+    reserve1
+    reserveUSD
+    volumeUSD
+    token0Price
+    token1Price
+    token0 {
+      id
+      symbol
+      name
+    }
+    token1 {
+      id
+      symbol
+      name
+    }
+  }`
+}
+
+const fetchPoolData = async (
+  block24h: number,
+  block48h: number,
+  block7d: number,
+  block14d: number,
+  poolAddresses: string[]
+) => {
+  try {
+    const query = gql`
+      query pools {
+        now: ${POOL_AT_BLOCK(null, poolAddresses)}
+        oneDayAgo: ${POOL_AT_BLOCK(block24h, poolAddresses)}
+        twoDaysAgo: ${POOL_AT_BLOCK(block48h, poolAddresses)}
+        oneWeekAgo: ${POOL_AT_BLOCK(block7d, poolAddresses)}
+        twoWeeksAgo: ${POOL_AT_BLOCK(block14d, poolAddresses)}
+      }
+    `
+    const data = await request<PoolsQueryResponse>(INFO_CLIENT, query)
+    return { data, error: false }
+  } catch (error) {
+    console.error('Failed to fetch pool data', error)
+    return { erro: true }
+  }
+}
+
+// Transforms pools into "0xADDRESS: { ...PoolFields }" format and cast strigns to numbers
+const parsePoolData = (pairs?: PoolFields[]) => {
+  if (!pairs) {
+    return {}
+  }
+  return pairs.reduce((accum: { [address: string]: FormattedPoolFields }, poolData) => {
+    const { volumeUSD, reserveUSD, reserve0, reserve1, token0Price, token1Price } = poolData
+    accum[poolData.id] = {
+      ...poolData,
+      volumeUSD: parseFloat(volumeUSD),
+      reserveUSD: parseFloat(reserveUSD),
+      reserve0: parseFloat(reserve0),
+      reserve1: parseFloat(reserve1),
+      token0Price: parseFloat(token0Price),
+      token1Price: parseFloat(token1Price),
+    }
+    return accum
+  }, {})
+}
+
+interface PoolDatas {
+  error: boolean
+  data?: {
+    [address: string]: PoolData
+  }
+}
+
+/**
+ * Fetch top pools by liquidity
+ */
+const usePoolDatas = (poolAddresses: string[]): PoolDatas => {
+  const [fetchState, setFetchState] = useState<PoolDatas>({ error: false })
+  const [t24h, t48h, t7d, t14d] = getDeltaTimestamps()
+  const { blocks, error: blockError } = useBlocksFromTimestamps([t24h, t48h, t7d, t14d])
+  const [block24h, block48h, block7d, block14d] = blocks ?? []
+
+  useEffect(() => {
+    const fetch = async () => {
+      const { error, data } = await fetchPoolData(
+        block24h.number,
+        block48h.number,
+        block7d.number,
+        block14d.number,
+        poolAddresses
+      )
+      if (error) {
+        setFetchState({ error: true })
+      } else {
+        const formattedPoolData = parsePoolData(data?.now)
+        const formattedPoolData24h = parsePoolData(data?.oneDayAgo)
+        const formattedPoolData48h = parsePoolData(data?.twoDaysAgo)
+        const formattedPoolData7d = parsePoolData(data?.oneWeekAgo)
+        const formattedPoolData14d = parsePoolData(data?.twoWeeksAgo)
+
+        // Calculate data and format
+        const formatted = poolAddresses.reduce((accum: { [address: string]: PoolData }, address) => {
+          // Undefined data is possible if pool is brand new and didn't exist one day ago or week ago.
+          const current: FormattedPoolFields | undefined = formattedPoolData[address]
+          const oneDay: FormattedPoolFields | undefined = formattedPoolData24h[address]
+          const twoDays: FormattedPoolFields | undefined = formattedPoolData48h[address]
+          const week: FormattedPoolFields | undefined = formattedPoolData7d[address]
+          const twoWeeks: FormattedPoolFields | undefined = formattedPoolData14d[address]
+
+          const [volumeUSD, volumeUSDChange] = getChangeForPeriod(
+            current?.volumeUSD,
+            oneDay?.volumeUSD,
+            twoDays?.volumeUSD
+          )
+          const [volumeUSDWeek, volumeUSDChangeWeek] = getChangeForPeriod(
+            current?.volumeUSD,
+            week?.volumeUSD,
+            twoWeeks?.volumeUSD
+          )
+
+          const liquidityUSD = current ? current.reserveUSD : 0
+
+          const liquidityUSDChange = getPercentChange(current?.reserveUSD, oneDay?.reserveUSD)
+
+          const liquidityToken0 = current ? current.reserve0 : 0
+          const liquidityToken1 = current ? current.reserve1 : 0
+
+          const { totalFees24h, totalFees7d, lpFees24h, lpFees7d, lpApr7d } = getLpFeesAndApr(
+            volumeUSD,
+            volumeUSDWeek,
+            liquidityUSD
+          )
+
+          if (current) {
+            accum[address] = {
+              address,
+              token0: {
+                address: current.token0.id,
+                name: current.token0.name,
+                symbol: current.token0.symbol,
+              },
+              token1: {
+                address: current.token1.id,
+                name: current.token1.name,
+                symbol: current.token1.symbol,
+              },
+              token0Price: current.token0Price,
+              token1Price: current.token1Price,
+              volumeUSD,
+              volumeUSDChange,
+              volumeUSDWeek,
+              volumeUSDChangeWeek,
+              totalFees24h,
+              totalFees7d,
+              lpFees24h,
+              lpFees7d,
+              lpApr7d,
+              liquidityUSD,
+              liquidityUSDChange,
+              liquidityToken0,
+              liquidityToken1,
+            }
+          }
+
+          return accum
+        }, {})
+        setFetchState({ data: formatted, error: false })
+      }
+    }
+
+    const allBlocksAvailable = block24h?.number && block48h?.number && block7d?.number && block14d?.number
+    if (poolAddresses.length > 0 && allBlocksAvailable && !blockError) {
+      fetch()
+    }
+  }, [poolAddresses, block24h, block48h, block7d, block14d, blockError])
+
+  return fetchState
+}
+
+export default usePoolDatas
